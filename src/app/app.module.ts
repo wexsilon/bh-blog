bb5f3e9b55a01caf5dd3d50be165d83f2f820336
wexsilon <wexsilon2001@protonmail.com>
2023-11-04 01:46:51 +0330

    add modules to imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { AuthModule } from 'src/auth/auth.module';
+import { PostModule } from 'src/post/post.module';
+import { SettingModule } from 'src/setting/setting.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
     imports: [
         ServeStaticModule.forRoot({
-            rootPath: join(__dirname, '..', 'static'),
+            rootPath: join(__dirname, '..', '..', 'static'),
             serveRoot: '/static'
         }),
+        AuthModule,
+        PostModule,
+        SettingModule,
+        UserModule
     ],
     controllers: [AppController],
     providers: [AppService],
