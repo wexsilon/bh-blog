43b4bf9f2a59334763a605998fcd43a48e32e565
wexsilon <wexsilon2001@protonmail.com>
2023-11-04 01:19:17 +0330

    setup morgan to log routes

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import morgan from 'morgan';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
+    app.use(morgan('dev'))
     app.enableCors();
     app.setBaseViewsDir(join(__dirname, '..', 'views'));
     app.setViewEngine('pug');
