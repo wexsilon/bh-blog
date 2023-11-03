b141268529df1b81a8769a220545b2b1423c1779
wexsilon <wexsilon2001@protonmail.com>
2023-11-04 01:34:02 +0330

    change name handler home page

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
     constructor(private readonly appService: AppService) {}
 
     @Get()
-    getHello(@Res() res: Response) {
+    homeGet(@Res() res: Response) {
         res.render('index.pug', { });
     }
 }
