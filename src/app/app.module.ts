676112819ea257f8b0940e7375a47d2e94ee0ba7
wexsilon <wexsilon2001@protonmail.com>
2023-11-04 02:06:16 +0330

    setup typeorm and database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,19 @@ import { AuthModule } from 'src/auth/auth.module';
 import { PostModule } from 'src/post/post.module';
 import { SettingModule } from 'src/setting/setting.module';
 import { UserModule } from 'src/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
     imports: [
         ServeStaticModule.forRoot({
             rootPath: join(__dirname, '..', '..', 'static'),
             serveRoot: '/static'
+        }),
+        TypeOrmModule.forRoot({
+            type: 'sqlite',
+            database: 'bh-blog.db',
+            entities: [],
+            synchronize: true,
         }),
         AuthModule,
         PostModule,
