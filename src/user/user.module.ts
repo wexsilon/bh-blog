3b9065ccfb71d99c60c65d2e3205e7707ee48bcf
wexsilon <wexsilon2001@protonmail.com>
2023-11-04 21:29:51 +0330

    move service to providers folder

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
+import { UserService } from './providers/user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
-  providers: [UserService]
+    imports: [TypeOrmModule.forFeature([User])],
+    providers: [UserService],
+    exports: [UserService],
 })
 export class UserModule {}
